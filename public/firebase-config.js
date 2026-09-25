ec0e80163840d724b8bc2eb37068d00057f7db1e
guerrasur <lucioguerrasur@gmail.com>
2026-09-25 18:18:27 -0300

    feat: add RifaVital app files

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1cPNoPYDKx-iziUmlSO8RxH7sobZAltM",
+  authDomain: "rifavital.firebaseapp.com",
+  projectId: "rifavital",
+  storageBucket: "rifavital.firebasestorage.app",
+  messagingSenderId: "624487969094",
+  appId: "1:624487969094:web:8cb480f099621d2a4b3547",
+  measurementId: "G-SE11PRDXFE"
+};
+
+export const ADMIN_UID = "9QDw54PNWUavURs7ti3RlYNXPMx2";
